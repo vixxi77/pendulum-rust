3fc6289131f1aee4738be24f3c5ee78308eb8684
yaye <viktorastankus37@gmail.com>
2026-04-20 20:44:21 +0300

    added raylib crate, raylib window boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use raylib::prelude::*;
+
 fn main() {
     println!("Rust pendulum \n");
+
+    let (mut rl, thread) = raylib::init()
+        .size(640, 480)
+        .title("Hello, World")
+        .build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::WHITE);
+        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    }
 }
